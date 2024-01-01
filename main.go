c88441a8c8c486a38d34605db023edbcb84b87a0
yui <nhankiet0501@gmail.com>
2024-01-01 17:37:18 +0700

    trying to use middleware

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
+type apiConfig struct {
+	fileserverHits int
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits++
+		next.ServeHTTP(w, r)
+	})
+}
+func middlewareLog(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s", r.Method, r.URL.Path)
+		next.ServeHTTP(w, r)
+	})
+}
 func middlewareCors(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
@@ -15,11 +35,38 @@ func middlewareCors(next http.Handler) http.Handler {
 	})
 }
 
-func main() {
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
 
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	_, err := w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits)))
+	if err != nil {
+		return
+	}
+}
+
+func (cfg *apiConfig) handlerResetMetrics(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	cfg.fileserverHits = 0
+}
+
+func main() {
+	cfg := &apiConfig{fileserverHits: 0}
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
-	corsMux := middlewareCors(mux)
+	mux.Handle("/app/", cfg.middlewareMetricsInc(
+		http.StripPrefix("/app",
+			http.FileServer(
+				http.Dir(".")))))
+	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.HandleFunc("/metrics", cfg.handlerMetrics)
+	mux.HandleFunc("/reset", cfg.handlerResetMetrics)
+	logMux := middlewareLog(mux)
+	corsMux := middlewareCors(logMux)
 
 	srv := &http.Server{
 		Addr:    ":8080",
