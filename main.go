2401923b9f11164ddcfad206ce10a2b9b46a512b
yui <nhankiet0501@gmail.com>
2024-01-01 11:50:30 +0700

    initialize server with CORS headers

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import "net/http"
+
+func middlewareCors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
+func main() {
+	mux := http.NewServeMux()
+	corsMux := middlewareCors(mux)
+	http.HandleFunc("/", corsMux.ServeHTTP)
+	err := http.ListenAndServe("localhost:8080", nil)
+	if err != nil {
+		return
+	}
+}
